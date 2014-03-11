ca5337b19f0a468baf53870565dae57d39ba9b88
Whitney Young <wbyoung@fadingred.com>
2014-03-11 16:15:59 -0700

    Abstracting module definition.

--- README.md
+++ README.md
@@ -1,83 +1,49 @@
-# gulp-emblem [![NPM version][npm-image]][npm-url] [![Build status][travis-image]][travis-url] [![Dependency Status][gemnasium-image]][gemnasium-url]
+# gulp-emblem [![NPM version][npm-image]][npm-url] [![Build status][travis-image]][travis-url]
 > [Emblem.js][emblem-url] plugin for gulp
 > ...a concise, beautiful, and fully compatible templating alternative for Handlebars.js.
 
 ## Usage
 
-First, install `gulp-emblem` as a development dependency:
+First, install _gulp-emblem_ and [gulp-define-module] as development dependencies:
 
 ```shell
-npm install --save-dev gulp-emblem
+npm install --save-dev gulp-emblem gulp-define-module
 ```
 
 Then, add it to your `gulpfile.js`:
 
 ```javascript
 var emblem = require('gulp-emblem');
+var defineModule = require('gulp-define-module');
 
 gulp.task('templates', function(){
   gulp.src(['client/templates/*.em'])
-    .pipe(emblem({
-      namespace: 'MyApp.templates',
-      outputType: 'hybrid'
-     }))
-    .pipe(concat('templates.js'))
-    .pipe(gulp.dest('build/js/'));
+    .pipe(emblem())
+    .pipe(defineModule('node'))
+    .pipe(gulp.dest('build/templates/'));
 });
 ```
+
+gulp-emblem outputs a raw handlebars function, so it is likely that you will want to use [gulp-define-module] to make the handlebars template available via a specific namespace or for use with a module system. For additional usage examples, we recommend that you visit [gulp-handlebars] and [gulp-define-module].
+
+
+## Compiling to various module systems
+
+[gulp-define-module] can be used to prepare the output for use with common module systems such as AMD, Node, and CommonJS. Please see the documentation for more details on how to use that in a gulp chain.
+
+gulp-emblem makes the following available for use in the [define-module wrapper](https://github.com/wbyoung/gulp-define-module#optionswrapper):
+
+ - `handlebars`: The handlebars template fully wrapped (`Handlebars.template(<%= contents %>)`).
+
 
 ## API
 
 ### emblem(options)
 
-#### options.namespace
-Type: `String`
-Default: `templates`
-
-The namespace in which the precompiled templates will be assigned. Use dot notation (e.g. `App.Templates`) for nested namespaces or false to declare templates in the global namespace.
-
-#### options.outputType
-Type: `String`
-Default: `browser`
-
-The desired output type. One of the following:
-
-* `browser` - Produce plain JavaScript files for the browser
-* `hybrid` - Produce Node modules that can optionally be used on the frontend
-* `node` - Produce Node modules
-* `amd` - Produce AMD modules
-* `commonjs` - Produce CommonJS modules
-* `bare` - Return an unmolested function definition
-
-#### options.declareNamespace
-Type: `Boolean`
-Default: `true`
-
-If true, non-destructively declare all parts of the namespace. This option is only necessary when `options.type` is `browser` or `hybrid`.
-
-For example, if the namespace is `MyApp.Templates` and a template is named `App.Header.hbs`, the following declaration will be present in the output file for that template:
-
-```javascript
-this["MyApp"] = this["MyApp"] || {};
-this["MyApp"]["templates"] = this["MyApp"]["templates"] || {};
-this["MyApp"]["templates"]["App"] = this["MyApp"]["templates"]["App"] || {};
-this["MyApp"]["templates"]["App"]["Header"] = function () {};
-```
-
-When processing multiple templates under a given namespace, this will result in duplicate declarations. That is, the non-destructive declaration of the namespace will be repeated for each template compiled.
-
-#### options.processName
-Type: `Function`
-Default: Strip file extension
-
-This option accepts a function which takes one argument (the template filepath) and returns a string which will be used as the key for the precompiled template object. By default, the filename minus the extension is used.
-
-If this function returns a string containing periods (not including the file extension), they will be represented as a sub-namespace. See `options.declareNamespace` for an example of the effect.
-
 #### options.compilerOptions
 Type: `Object`
 
-Compiler options to pass to `Handlebars.precompile()`.
+Compiler options to pass to `Emblem.precompile()`.
 
 ## Attributions
 This project is a fork of [`gulp-handlebars`](https://github.com/lazd/gulp-handlebars) by [lazd](https://github.com/lazd).
@@ -88,7 +54,7 @@ This project is a fork of [`gulp-handlebars`](https://github.com/lazd/gulp-handl
 [travis-image]: https://secure.travis-ci.org/Aulito/gulp-emblem.png?branch=master
 [npm-url]: https://npmjs.org/package/gulp-emblem
 [npm-image]: https://badge.fury.io/js/gulp-emblem.png
-[gemnasium-image]: https://gemnasium.com/Aulito/gulp-emblem.png
-[gemnasium-url]: https://gemnasium.com/Aulito/gulp-emblem
 
+[gulp-define-module]: https://github.com/wbyoung/gulp-define-module
+[gulp-handlebars]: https://github.com/lazd/gulp-handlebars
 [emblem-url]: http://emblemjs.com/
--- test/main.js
+++ test/main.js
@@ -1,4 +1,4 @@
-var handlebarsPlugin = require('../');
+var emblemPlugin = require('../');
 var should = require('should');
 var gutil = require('gulp-util');
 var os = require('os');
@@ -24,205 +24,21 @@ var fileMatchesExpected = function(file, expectedFileName) {
     String(file.contents).should.equal(getExpectedString(expectedFileName));
 };
 
-describe('gulp-handlebars', function() {
-  describe('handlebarsPlugin()', function() {
+describe('gulp-emblem', function() {
+  describe('emblemPlugin()', function() {
 
-    it('should declare namespaces', function(done) {
-      var stream = handlebarsPlugin({
-        outputType: 'browser',
-        namespace: 'MyApp.Templates',
-        declareNamespace: true
-      });
-
-      var appTemplate = new gutil.File({
-        base: 'fixtures',
-        path: path.join('fixtures','App.Main.hbs'),
-        contents: new Buffer('<div class="app"></div>')
-      });
-
-      stream.on('data', function(newFile) {
-        should.exist(newFile);
-        should.exist(newFile.contents);
-        var contents = String(newFile.contents);
-        contents.slice(0, 98).should.equal('this["MyApp"] = this["MyApp"] || {};this["MyApp"]["Templates"] = this["MyApp"]["Templates"] || {};');
-        done();
-      });
-      stream.write(appTemplate);
-      stream.end();
-    });
-
-    it('should declare the namespace for templates', function(done) {
-      var stream = handlebarsPlugin({
-        outputType: 'browser',
-        namespace: 'MyApp.Templates',
-        declareNamespace: true
-      });
-
-      var appTemplate = new gutil.File({
-        base: 'fixtures',
-        path: path.join('fixtures','App.Main.hbs'),
-        contents: new Buffer('<div class="app"></div>')
-      });
-
-      stream.on('data', function(newFile) {
-        should.exist(newFile);
-        should.exist(newFile.contents);
-        var contents = String(newFile.contents);
-        contents.slice(0,174).should.equal('this["MyApp"] = this["MyApp"] || {};this["MyApp"]["Templates"] = this["MyApp"]["Templates"] || {};this["MyApp"]["Templates"]["App"] = this["MyApp"]["Templates"]["App"] || {};');
-        done();
-      });
-      stream.write(appTemplate);
-      stream.end();
-    });
-
-    it('should compile unwrapped bare templates', function(done) {
-      var stream = handlebarsPlugin({
-        outputType: 'bare',
-        wrapped: false,
-        namespace: false
-      });
-
+    it('should compile templates', function(done) {
+      var stream = emblemPlugin();
       var basicTemplate = getFixture('Basic.hbs');
 
       stream.on('data', function(newFile) {
         should.exist(newFile);
         should.exist(newFile.contents);
-        fileMatchesExpected(newFile, 'Basic_bare_unwrapped.js');
+        fileMatchesExpected(newFile, 'Basic.js');
         done();
       });
       stream.write(basicTemplate);
       stream.end();
     });
-
-    it('should compile wrapped bare templates', function(done) {
-      var stream = handlebarsPlugin({
-        outputType: 'bare',
-        wrapped: true,
-        namespace: false
-      });
-
-      var basicTemplate = getFixture('Basic.hbs');
-
-      stream.on('data', function(newFile) {
-        should.exist(newFile);
-        should.exist(newFile.contents);
-        fileMatchesExpected(newFile, 'Basic_bare.js');
-        done();
-      });
-      stream.write(basicTemplate);
-      stream.end();
-    });
-
-    it('should compile templates for the browser', function(done) {
-      var stream = handlebarsPlugin({
-        outputType: 'browser',
-        namespace: false
-      });
-
-      var basicTemplate = getFixture('Basic.hbs');
-
-      stream.on('data', function(newFile) {
-        should.exist(newFile);
-        should.exist(newFile.contents);
-        fileMatchesExpected(newFile, 'Basic_browser.js');
-        done();
-      });
-      stream.write(basicTemplate);
-      stream.end();
-    });
-
-    it('should compile templates for AMD', function(done) {
-      var stream = handlebarsPlugin({
-        outputType: 'amd',
-        namespace: false
-      });
-
-      var basicTemplate = getFixture('Basic.hbs');
-
-      stream.on('data', function(newFile) {
-        should.exist(newFile);
-        should.exist(newFile.contents);
-        fileMatchesExpected(newFile, 'Basic_amd.js');
-        done();
-      });
-      stream.write(basicTemplate);
-      stream.end();
-    });
-
-    it('should compile templates for CommonJS', function(done) {
-      var stream = handlebarsPlugin({
-        outputType: 'commonjs',
-        namespace: false
-      });
-
-      var basicTemplate = getFixture('Basic.hbs');
-
-      stream.on('data', function(newFile) {
-        should.exist(newFile);
-        should.exist(newFile.contents);
-        fileMatchesExpected(newFile, 'Basic_commonjs.js');
-        done();
-      });
-      stream.write(basicTemplate);
-      stream.end();
-    });
-
-    it('should compile templates for Node', function(done) {
-      var stream = handlebarsPlugin({
-        outputType: 'node',
-        namespace: false
-      });
-
-      var basicTemplate = getFixture('Basic.hbs');
-
-      stream.on('data', function(newFile) {
-        should.exist(newFile);
-        should.exist(newFile.contents);
-        fileMatchesExpected(newFile, 'Basic_node.js');
-        done();
-      });
-      stream.write(basicTemplate);
-      stream.end();
-    });
-
-    it('should compile hybrid Node/browser templates', function(done) {
-      var stream = handlebarsPlugin({
-        outputType: 'hybrid',
-        namespace: false
-      });
-
-      var basicTemplate = getFixture('Basic.hbs');
-
-      stream.on('data', function(newFile) {
-        should.exist(newFile);
-        should.exist(newFile.contents);
-        fileMatchesExpected(newFile, 'Basic_hybrid.js');
-        done();
-      });
-      stream.write(basicTemplate);
-      stream.end();
-    });
-
-    it('should support custom processName functions', function(done) {
-      var stream = handlebarsPlugin({
-        outputType: 'browser',
-        namespace: false,
-        processName: function(name) {
-          return 'x';
-        }
-      });
-
-      var basicTemplate = getFixture('Basic.hbs');
-
-      stream.on('data', function(newFile) {
-        should.exist(newFile);
-        should.exist(newFile.path);
-        newFile.path.should.equal('test/fixtures/x.js');
-        done();
-      });
-      stream.write(basicTemplate);
-      stream.end();
-    });
-
   });
 });
